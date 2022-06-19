c34e9b7ed7ea31aed3e616e7057125da2f60b1ac
Linjey-git <lingey.ubi@gmail.com>
2022-06-19 17:40:58 +0200

    Initial commit

---
+++ src/main/java/net/seho/omnia/OmniaModClient.java
@@ -0,0 +1,10 @@
+package net.seho.omnia;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OmniaModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
